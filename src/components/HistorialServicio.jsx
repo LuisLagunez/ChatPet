6360e34dc6947eb37f5e205e167208523838a104
LeticiaTejeroGamboa2401 <leticia.tejero.gamboa@hotmail.com>
2025-05-12 04:06:06 -0600

    historial

--- src/components/HistorialServicio.jsx
+++ src/components/HistorialServicio.jsx
@@ -3,7 +3,7 @@ import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
-import { Typography } from '@mui/material';
+import { Typography, Avatar } from '@mui/material';
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: '#fff',
@@ -14,7 +14,23 @@ const Item = styled(Paper)(({ theme }) => ({
     ...theme.applyStyles('dark', {
       backgroundColor: '#1A2027',
     }),
-  }));
+}));
+
+function createDataHistorial(
+    cliente,
+    fecha,
+    hora,
+    lugar,
+    mascota,
+    pago
+) {
+    return {cliente, fecha, hora, lugar, mascota, pago};
+}
+
+const rowsHistorial = [
+    createDataHistorial('Alberto-Dalesio', '30/04/2025', '14:00', 'A domicilio', 'Pugberto', 250),
+    createDataHistorial('Lisa', '28/04/2025', '13:00', 'A domicilio', 'Love', 450)
+]
 
 export function HistorialServicio(){
     return (
@@ -35,27 +51,88 @@ export function HistorialServicio(){
                             borderRadius: '10px',
                             boxShadow: 2,
                             display: 'flex',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            alignItems: 'start',
-                            pt:2
+                            flexDirection: 'column',
+                            alignItems: 'start', 
+                            pt: 2,
+                            paddingLeft:4
                         }}
                     >
                         <Box
                             sx={{
                                 bgcolor: '#FFFFFF',
                                 width: '95%',
-                                height: '7vh',
+                                height: '5vh',
                                 borderRadius: '10px',
-                                border: 3,
-                                borderColor: '#A4A4A4',
+                                border: 2,
+                                borderColor: 'black',
                                 display: 'flex',
                                 alignItems: 'center',
-                                justifyContent: 'center'
+                                px: 2,
+                                flexWrap: 'wrap',
+                                marginBottom:2
                             }}
                         >
-                            Algo che
+                            <Typography sx={{ fontSize: '16px', flex: 1, minWidth: '130px' }}>Cliente</Typography>
+                            <Typography sx={{ fontSize: '16px', flex: 1, minWidth: '130px' }}>Fecha y hora</Typography>
+                            <Typography sx={{ fontSize: '16px', flex: 1, minWidth: '100px' }}>Lugar</Typography>
+                            <Typography sx={{ fontSize: '16px', flex: 1, minWidth: '100px' }}>Mascota</Typography>
+                            <Typography sx={{ fontSize: '16px', flex: 1, minWidth: '100px' }}>Pago</Typography>
                         </Box>
+
+                        {rowsHistorial.map((rowHistorial) => (
+                            <Box
+                                sx={{
+                                    backgroundImage: 'linear-gradient(to right, #969696 1%, #FFFFFF 1%)',
+                                    width: '95%',
+                                    height: '10vh',
+                                    borderRadius: '15px',
+                                    border: 2,
+                                    borderColor:'#969696',
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    px:2,
+                                    flexWrap: 'wrap',
+                                    marginTop: 1
+                                }}
+                            >
+                                <Box sx={{ position: 'relative', display: 'inline-block', marginRight: 1, marginRight:'13%'}}>
+                                    {/* Avatar grande */}
+                                    <Avatar 
+                                        alt={rowHistorial.cliente} 
+                                        src={`/${rowHistorial.cliente}.jpg`} 
+                                        sx={{ width: 70, height: 70 }} 
+                                    />
+
+                                    {/* Avatar pequeño superpuesto */}
+                                    <Avatar 
+                                        alt={rowHistorial.mascota} 
+                                        src={`/${rowHistorial.mascota}.jpg`} 
+                                        sx={{ 
+                                        width: 35, 
+                                        height: 35, 
+                                        position: 'absolute',
+                                        bottom: 0,
+                                        right: 0,
+                                        }} 
+                                    />
+                                </Box>
+                                <Typography sx={{ fontSize: '16px', flex: 1, minWidth: '130px' }}>{rowHistorial.fecha}<br/>{rowHistorial.hora} hrs.</Typography>
+                                <Typography sx={{ fontSize: '16px', flex: 1, minWidth: '100px' }}>{rowHistorial.lugar}</Typography>
+                                <Typography sx={{ fontSize: '16px', flex: 1, minWidth: '100px' }}>{rowHistorial.mascota}</Typography>
+                                <Typography sx={{ 
+                                    fontSize: '35px', 
+                                    flex: 1, 
+                                    minWidth: '100px',
+                                    color: '#A6D993',
+                                    WebkitTextStroke: '1px #5D8C4C',
+                                    fontWeight: 'bold',
+                                    }}
+                                >
+                                    ${rowHistorial.pago}
+                                </Typography>
+                            </Box>
+                        ))}
+
                     </Box>
                 </Grid>
             </Grid>
